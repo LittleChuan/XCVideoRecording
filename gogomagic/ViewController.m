ddfcbf19943f4ee3c7ecb4b2b24bb380aecd1c11
Chuan <248254119@qq.com>
2016-04-14 16:55:02 +0800

    initial commit

--- gogomagic/ViewController.m
+++ gogomagic/ViewController.m
@@ -8,20 +8,95 @@
 
 #import "ViewController.h"
 
+#import <GPUImage/GPUImage.h>
+#import <ZFPlayer/ZFPlayer.h>
+#import <YTXVideoAVPlayer/YTXVideoAVPlayerView.h>
+
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet GPUImageView *imageView;
+@property (weak, nonatomic) IBOutlet YTXVideoAVPlayerView *ytx_player;
+@property (weak, nonatomic) IBOutlet ZFPlayerView *zf_player;
+
+@property (nonatomic, strong) GPUImageVideoCamera *videoCamera;
+@property (nonatomic, strong) GPUImageSepiaFilter *filter;
+@property (nonatomic, strong) GPUImageMovieWriter *movieWriter;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (BOOL)prefersStatusBarHidden {
+    return YES;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [UIApplication sharedApplication].statusBarHidden = YES;
+    
+    self.videoCamera = [[GPUImageVideoCamera alloc] initWithSessionPreset:AVCaptureSessionPreset640x480 cameraPosition:AVCaptureDevicePositionBack];
+    //    videoCamera = [[GPUImageVideoCamera alloc] initWithSessionPreset:AVCaptureSessionPreset640x480 cameraPosition:AVCaptureDevicePositionFront];
+    //    videoCamera = [[GPUImageVideoCamera alloc] initWithSessionPreset:AVCaptureSessionPreset1280x720 cameraPosition:AVCaptureDevicePositionBack];
+    //    videoCamera = [[GPUImageVideoCamera alloc] initWithSessionPreset:AVCaptureSessionPreset1920x1080 cameraPosition:AVCaptureDevicePositionBack];
+    self.videoCamera.outputImageOrientation = UIInterfaceOrientationPortrait;
+    self.videoCamera.horizontallyMirrorFrontFacingCamera = NO;
+    self.videoCamera.horizontallyMirrorRearFacingCamera = NO;
+    
+    self.filter = [[GPUImageSepiaFilter alloc] init];
+    
+    //    filter = [[GPUImageTiltShiftFilter alloc] init];
+    //    [(GPUImageTiltShiftFilter *)filter setTopFocusLevel:0.65];
+    //    [(GPUImageTiltShiftFilter *)filter setBottomFocusLevel:0.85];
+    //    [(GPUImageTiltShiftFilter *)filter setBlurSize:1.5];
+    //    [(GPUImageTiltShiftFilter *)filter setFocusFallOffRate:0.2];
+    
+    //    filter = [[GPUImageSketchFilter alloc] init];
+    //    filter = [[GPUImageColorInvertFilter alloc] init];
+    //    filter = [[GPUImageSmoothToonFilter alloc] init];
+    //    GPUImageRotationFilter *rotationFilter = [[GPUImageRotationFilter alloc] initWithRotation:kGPUImageRotateRightFlipVertical];
+    
+    [self.videoCamera addTarget:self.filter];
+    
+    NSString *pathToMovie = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/Movie.m4v"];
+    unlink([pathToMovie UTF8String]); // If a file already exists, AVAssetWriter won't let you record new frames, so delete the old movie
+    NSURL *movieURL = [NSURL fileURLWithPath:pathToMovie];
+    
+    self.movieWriter = [[GPUImageMovieWriter alloc] initWithMovieURL:movieURL size:CGSizeMake(480.0, 640.0)];
+    self.movieWriter.encodingLiveVideo = YES;
+    //    movieWriter = [[GPUImageMovieWriter alloc] initWithMovieURL:movieURL size:CGSizeMake(640.0, 480.0)];
+    //    movieWriter = [[GPUImageMovieWriter alloc] initWithMovieURL:movieURL size:CGSizeMake(720.0, 1280.0)];
+    //    movieWriter = [[GPUImageMovieWriter alloc] initWithMovieURL:movieURL size:CGSizeMake(1080.0, 1920.0)];
+    [self.filter addTarget:self.movieWriter];
+    [self.filter addTarget:self.imageView];
+    
+    self.imageView.fillMode = kGPUImageFillModePreserveAspectRatioAndFill;
+    
+    [self.videoCamera startCameraCapture];
 }
+
+- (IBAction)start:(id)sender {
+    [self.movieWriter startRecording];
+    NSLog(@"start recording");
+}
+
+
+
+- (IBAction)stop:(id)sender {
+    NSString *pathToMovie = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/Movie.m4v"];
+    NSURL *movieURL = [NSURL fileURLWithPath:pathToMovie];
+    [self.movieWriter finishRecording];
+    NSLog(@"finish recording");
+    
+    [self.ytx_player setURLString:movieURL.absoluteString];
+    [self.ytx_player play];
+    
+    self.zf_player.videoURL = movieURL;
+    self.zf_player.playerLayerGravity = ZFPlayerLayerGravityResizeAspectFill;
+}
+
+- (IBAction)delete:(id)sender {
+}
+
 
 @end
